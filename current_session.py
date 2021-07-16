9e88bf7177d4132e9edf3ba7154aeb528af7d2ee
O P <piwtorachok@gmail.com>
2021-07-16 19:29:45 +0300

    Added the first look session

---
+++ current_session.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+get_ipython().run_cell_magic('magic', '', '\n')
+print("Let's start working with the open data")
+get_ipython().run_cell_magic('save', 'current_session /-', '\n')
+get_ipython().run_line_magic('save', 'current_session /0')
+get_ipython().run_line_magic('save', 'current_session ')
+get_ipython().run_line_magic('save', 'current_session .')
+get_ipython().run_line_magic('save', 'current_session test.py')
+import os
+os.path.listdir(".")
+os.listdir(".")
+get_ipython().run_line_magic('save', 'current_session ~0/')
